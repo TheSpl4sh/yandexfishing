e250c1795bea81b9d37d48b2119cff86706d79dd
TheSpl4sh <thesplash007@gmail.com>
2025-01-30 21:42:10 +0200

    feat: input, ButtonComponent, ToggleButton without func, main layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,71 @@
 import { Footer } from "./components/Footer"
 import ToggleButton from "./components/ToggleButton"
 import YaIdLogo from './assets/YaIdLogo.png'
-import { FaArrowLeft } from "react-icons/fa";
+import vkImg from './assets/vkImg.svg'
+import googleImg from './assets/googleImg.svg'
+import facebookImg from './assets/facebookImg.svg'
+import qrImage from './assets/qrImage.svg'
+import moreImg from './assets/moreImg.svg'
+import { FaArrowLeft } from "react-icons/fa"
+import ButtonComponent from "./components/ButtonComponent";
+import { useState } from "react"
 
 const App = () => {
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-[#121212] text-white">
-            <div className="bg-[#1c1c1c] rounded-3xl p-5 w-11/12 max-w-md text-center">
-                <div className="flex items-center justify-between mb-4">
-                    <button className="text-xl"><FaArrowLeft /></button>
-                    <img className="max-w-40 text-wh invert" src={YaIdLogo}/>
-                    <h1></h1>
-                </div>
-                <h2 className="text-lg font-semibold mb-4">Войдите с Яндекс ID</h2>
-                <ToggleButton />
-                <div className="mt-5">
-                    <input 
-                        type="text" 
-                        placeholder="Логин или email" 
-                        className="w-full p-2 rounded-md border-2 border-[#272727] min-h-14 bg-[#1c1c1c] text-white placeholder-gray-400 outline-none focus:ring-2 focus:ring-gray-500"
+    const [placeholder, setPlaceholder] = useState("Логин или email")
+
+
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center gap-2 min-h-screen bg-[#121212] text-white">
+                <div className="bg-[#1c1c1c] rounded-3xl p-[32px] max-w-[412px] text-center">
+                    <div className="flex items-center justify-between mb-4">
+                        <button className="text-xl"><FaArrowLeft /></button>
+                        <img className="max-w-40 text-wh invert" src={YaIdLogo} />
+                        <h1></h1>
+                    </div>
+                    <h2 className="text-lg font-semibold mb-4">Войдите с Яндекс ID</h2>
+                    <ToggleButton setPlaceholder={setPlaceholder} />
+                    <div className="relative w-full mt-5">
+                        <button className="absolute left-2 top-1/2 transform -translate-y-1/2 flex items-center gap-1 bg-transparent text-white px-2 py-1 rounded-lg border border-gray-500">
+                            <img src="/flag-russia.png" alt="RU" className="w-6 h-4 rounded-sm" />
+                        </button>
+                        <input
+                            type="text"
+                            placeholder={placeholder}
+                            className="w-full p-3 mb-4 rounded-2xl border-[1px] border-[#d3d3de33] min-h-10 bg-[#1c1c1c] text-white placeholder-gray-400 outline-none focus:ring-2 focus:ring-gray-500 text-2xl"
+                        />
+                    </div>
+                    <ButtonComponent
+                        name="Войти"
+                        backGround="bg-white"
+                        textColor="text-[#1f1f24]"
                     />
+                    <ButtonComponent
+                        name="По лицу или отпечатку"
+                        backGround="bg-[#d3d3de33]"
+                        textColor="text-white"
+                    />
+                    <ButtonComponent
+                        name="Создать ID"
+                        backGround="bg-[#1c1c1c] "
+                        textColor="text-white"
+                    />
+                    <h3>Войти с помощью</h3>
+                    <div className="flex items-center justify-center gap-8 mt-5">
+                        <img src={vkImg} alt="VK" className="w-8 h-8 cursor-pointer" />
+                        <img src={googleImg} alt="Google" className="w-8 h-8 cursor-pointer" />
+                        <img src={qrImage} alt="QR-код" className="w-8 h-8 cursor-pointer" />
+                        <img src={facebookImg} alt="Facebook" className="w-8 h-8 cursor-pointer" />
+                        <img src={moreImg} alt="More" className="w-8 h-2 cursor-pointer" />
+                    </div>
                 </div>
-                <button className="w-full mt-4 p-2 rounded-md bg-white text-black hover:bg-gray-200">Войти</button>
-                <button className="w-full mt-2 p-2 rounded-md bg-gray-700 text-gray-400 hover:bg-gray-600">Создать ID</button>
-                <div className="flex justify-center gap-3 mt-5">
-                    <img src="vk-icon.png" alt="VK" className="w-8 h-8 cursor-pointer" />
-                    <img src="google-icon.png" alt="Google" className="w-8 h-8 cursor-pointer" />
-                    <img src="qr-icon.png" alt="QR-код" className="w-8 h-8 cursor-pointer" />
-                    <img src="more-icon.png" alt="More" className="w-8 h-8 cursor-pointer" />
+                <div className="text-center mt-5 text-sm text-gray-400">
+                    <p>Яндекс ID — ключ от всех сервисов<br /><a href="#" className="text-blue-500">Узнать больше</a></p>
+                    <p>Ru · Справка и поддержка · © 2001–2025, Яндекс</p>
                 </div>
             </div>
-            <div className="text-center mt-5 text-sm text-gray-400">
-                <p>Яндекс ID — ключ от всех сервисов<br /><a href="#" className="text-blue-500">Узнать больше</a></p>
-                <p>Ru · Справка и поддержка · © 2001–2025, Яндекс</p>
-            </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default App
--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,27 +1,33 @@
 import React, {useState} from 'react'
 
-const ToggleButton = () => {
+const ToggleButton = ({setPlaceholder}) => {
     const [active, setActive] = useState("email");
     
   return (
-    <div className="flex border-2 border-[#272727] min-h-14 rounded-2xl overflow-hidden p-0.5 bg-[#1c1c1c]">
+    <div className="flex border-[1px] border-[#d3d3de33] min-h-14 rounded-2xl overflow-hidden p-0.5 bg-[#1c1c1c]">
       <button
-        className={`flex-1 py-2 text-center text-sm font-semibold rounded-xl ${
+        className={`flex-1 py-2 text-center text-sm font-semibold rounded-xl hover:bg-[#d3d3de33] active:scale-95 ${
           active === "email"
-          ? "bg-[#272727] text-white"
+          ? "bg-[#d3d3de33] text-white"
           : "bg-[#1c1c1c] "
         }`}
-        onClick={() => setActive("email")}
+        onClick={() => {
+          setActive("email");
+          setPlaceholder("Логин или email");
+        }}
       >
         Почта
       </button>
       <button
-        className={`flex-1 py-2 text-center text-sm font-semibold rounded-xl ${
+        className={`flex-1 py-2 text-center text-sm font-semibold rounded-xl hover:bg-[#d3d3de33] active:scale-95 ${
           active === "phone"
-            ? "bg-[#272727] text-white"
+            ? "bg-[#d3d3de33] text-white"
             : "bg-[#1c1c1c] hover:text-white"
         }`}
-        onClick={() => setActive("phone")}
+        onClick={() => {
+          setActive("phone");
+          setPlaceholder("+7 (000) 000-00-00");
+        }}
       >
         Телефон
       </button>
